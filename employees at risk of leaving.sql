30bbdd3b16f7b91aa2916f73f38806619f40cf35
Jose Andres Alvarado <167814528+JoseAlvaradoG13@users.noreply.github.com>
2024-11-27 16:32:07 -0600

    Update employees at risk of leaving.sql

--- employees at risk of leaving.sql
+++ employees at risk of leaving.sql
@@ -1,15 +1,21 @@
 #Identify employees at risk of leaving
-#First I want to calculate the AVG Satisfaction on the company.alter
+  
+--I would like to check the average satisfaction level of employees who have already left the company.
+WITH Satisfaction AS (
+SELECT EmployeeNumber, EnvironmentSatisfaction, JobSatisfaction, RelationshipSatisfaction, JobInvolvement,
+(EnvironmentSatisfaction + JobSatisfaction + RelationshipSatisfaction + WorkLifeBalance + JobInvolvement) / 5 AS AVG_Satisfaction
+FROM employees.hr_employee_attrition
+WHERE Attrition = 'Yes')
+
+SELECT 
+ROUND(AVG(AVG_Satisfaction),2)
+FROM Satisfaction
+
+--The AVG_ Satisfaction of the employees that already left the company is 2.54, I would recomend first focus on employees with less ot equal level of AVG_ satisfaction.
 
 SELECT EmployeeNumber, EnvironmentSatisfaction, JobSatisfaction, RelationshipSatisfaction, JobInvolvement,
 (EnvironmentSatisfaction + JobSatisfaction + RelationshipSatisfaction + WorkLifeBalance + JobInvolvement) / 5 AS AVG_Satisfaction
 FROM employees.hr_employee_attrition
 WHERE Attrition = 'No'
-ORDER BY AVG_Satisfaction ASC
-
-#I want to check what AVG satisfaction does the employees have that already left the company
-SELECT EmployeeNumber, EnvironmentSatisfaction, JobSatisfaction, RelationshipSatisfaction, JobInvolvement,
-(EnvironmentSatisfaction + JobSatisfaction + RelationshipSatisfaction + WorkLifeBalance + JobInvolvement) / 5 AS AVG_Satisfaction
-FROM employees.hr_employee_attrition
-WHERE Attrition = 'Yes'
+AND (EnvironmentSatisfaction + JobSatisfaction + RelationshipSatisfaction + WorkLifeBalance + JobInvolvement) / 5 <= 2.54
 ORDER BY AVG_Satisfaction ASC
