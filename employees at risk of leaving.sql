9f0212675dedc3de6809ea8333519070c3564bd9
Jose Andres Alvarado <167814528+JoseAlvaradoG13@users.noreply.github.com>
2024-11-27 16:32:53 -0600

    Update employees at risk of leaving.sql

--- employees at risk of leaving.sql
+++ employees at risk of leaving.sql
@@ -1,6 +1,6 @@
 #Identify employees at risk of leaving
   
---I would like to check the average satisfaction level of employees who have already left the company.
+--I´m going to check the average satisfaction level of employees who have already left the company.
 WITH Satisfaction AS (
 SELECT EmployeeNumber, EnvironmentSatisfaction, JobSatisfaction, RelationshipSatisfaction, JobInvolvement,
 (EnvironmentSatisfaction + JobSatisfaction + RelationshipSatisfaction + WorkLifeBalance + JobInvolvement) / 5 AS AVG_Satisfaction
@@ -11,7 +11,7 @@ SELECT
 ROUND(AVG(AVG_Satisfaction),2)
 FROM Satisfaction
 
---The AVG_ Satisfaction of the employees that already left the company is 2.54, I would recomend first focus on employees with less ot equal level of AVG_ satisfaction.
+--The AVG_ Satisfaction of the employees that already left the company is 2.54, I would recommend first focus on employees with less ot equal level of AVG_ satisfaction.
 
 SELECT EmployeeNumber, EnvironmentSatisfaction, JobSatisfaction, RelationshipSatisfaction, JobInvolvement,
 (EnvironmentSatisfaction + JobSatisfaction + RelationshipSatisfaction + WorkLifeBalance + JobInvolvement) / 5 AS AVG_Satisfaction
