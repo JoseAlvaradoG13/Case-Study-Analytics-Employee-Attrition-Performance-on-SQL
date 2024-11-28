da79a2f2e3669331809124da1b38cfb0b555641b
Jose Andres Alvarado <167814528+JoseAlvaradoG13@users.noreply.github.com>
2024-11-28 15:42:10 -0600

    Update Standard hours.sql

--- Standard hours.sql
+++ Standard hours.sql
@@ -1,6 +1,6 @@
-#Standard Hours across employees
+--Standard Hours across employees
 SELECT 
 DISTINCT StandardHours
 FROM employees.hr_employee_attrition; 
 
-#All the employees works the same 80 hours at month, I´m missing how many extra hours the employees works, so I can´t calculate it.
+--All the employees works the same 80 hours at month, I´m missing how many extra hours the employees works, so I can´t calculate it.
