4f2ff44d7a356ce58a3deb2fed9ed6491f2e3707
Jose Andres Alvarado <167814528+JoseAlvaradoG13@users.noreply.github.com>
2024-11-28 12:34:17 -0600

    Update Work Life Balance.sql

--- Work Life Balance.sql
+++ Work Life Balance.sql
@@ -19,13 +19,10 @@ WHERE Attrition = 'No'
 AND WorkLifeBalance <= 3
 GROUP BY JobSatisfaction
 ORDER BY JobSatisfaction ASC
-
 --Using the last table, we calculated the percentage of employees with a Work-Life Balance of 3 or less, categorized by their Job Satisfaction levels. 
 --The results are as follows: 18.07% for a Job Satisfaction of 1, 19.24% for a Job Satisfaction of 2, 29.45% for a Job Satisfaction of 3, and 33.24% for a Job Satisfaction of 4.
-
 --Sum the porcentage with Job Satisfaction 3 or lower
 SELECT
 (18.07 + 19.24 + 29.45) AS Low_Level_Job_Satis
-
 --Based on the results, 66.76% of employees with a Work-Life Balance of 3 or less are dissatisfied with their current Job Satisfaction. 
 --This could be one of the main reasons for their lower level of Work-Life Balance.
